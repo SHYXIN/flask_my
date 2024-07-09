271f47ddc24bb02c0d4f1c2629b75892a615c116
SHYXIN <55689295+SHYXIN@users.noreply.github.com>
2024-07-09 23:54:35 +0800

    第14章 Application Programming Interfaces

--- app/fake.py
+++ app/fake.py
@@ -2,21 +2,27 @@ from random import randint
 from sqlalchemy.exc import IntegrityError
 from faker import Faker
 from . import db
-from .models import User, Post
+from .models import User, Post, Role
 
 def users(count=100):
     # fake = Faker()
+    Role.insert_roles()
+    roles = Role.query.all()[::-1]
     fake = Faker('zh_CN')
     i = 0
     while i < count:
-        u = User(email=fake.email(),
-                 username=fake.user_name(),
+        u = User(
+                # email=fake.email(),
+                email=f'wangxin_{i}@qq.com',
+                #  username=fake.user_name(),
+                 username=f'wangxin_{i}',
                  password='password',
                  confirmed=True,
                  name=fake.name(),
                  location=fake.city(),
                  about_me=fake.text(),
                  member_since=fake.past_date())
+        u.role = roles[i % 3]
         db.session.add(u)
         try:
             db.session.commit()
