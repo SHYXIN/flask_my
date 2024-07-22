417145c6d0e0fbb66a317f4c5ae83f999cf91691
Py_TaiYuan_WangXin <wangxin@zhulong.com.cn>
2024-07-22 16:50:17 +0800

    Chapter 17: Email notification of application errors (17b)

--- flasky.py
+++ flasky.py
@@ -10,7 +10,7 @@ import sys
 import click
 from app import create_app, db
 from app.models import User, Role, Permission, Post, Follow, Comment
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -54,3 +54,16 @@ def profile(length, profile_dir):
     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
                                       profile_dir=profile_dir)
     app.run(debug=False)
+
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
+
+    # create or update user roles
+    Role.insert_roles()
+
+    # ensure all users are following themselves
+    User.add_self_follows()
